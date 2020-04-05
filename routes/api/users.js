ef7d66d6daa4d8e54e61e9ef0ba85806f71a81e2
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2020-04-05 10:34:07 +0200

    refactor to array, clean code

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,18 +2,20 @@ const express = require("express");
 const router = express.Router();
 const { check, validationResult } = require("express-validator");
 
-/*
-  @rout POST api/users
-  @desc Register user
-  @access Public
-*/
-router.post("/", [
+const validateInputs = [
   check('name', 'name is required')
     .not()
     .isEmpty(),
   check('email', 'please include a valid email').isEmail(),
   check('password', 'please enter a password with 6 or more characters').isLength({ min: 6 })
-], (req, res) => {
+];
+
+/*
+  @rout POST api/users
+  @desc Register user
+  @access Public
+*/
+router.post("/", validateInputs, (req, res) => {
   const errors = validationResult(req);
   if(!errors.isEmpty()) {
     return res.status(400).json({ errors: errors.array() });
