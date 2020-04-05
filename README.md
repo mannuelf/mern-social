a405085305415f2bb1e923b6a17cb62911ca6363
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2020-04-05 09:42:45 +0200

    readmre, api docs postman

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # MERN Social
 
 Project to build on and learn the MERN Stack.
+
+## API
+
+API Endpoints and documentation can be viewed here:
+
+[Click here to read](https://documenter.getpostman.com/view/4217/SzYbyci4)
--- server.js
+++ server.js
@@ -2,17 +2,15 @@ const express = require('express');
 require("dotenv").config();
 const connectDB = require("./config/db");
 const app = express();
-
-connectDB();
-
 const PORT = process.env.PORT || 5000;
+connectDB();
 
 app.get("/", (req, res) => res.send("API Running"));
 
 // Routes
-app.use("/api/users", require("./routes/api/users"));
 app.use("/api/auth", require("./routes/api/auth"));
-app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/posts", require("./routes/api/posts"));
+app.use("/api/profile", require("./routes/api/profile"));
+app.use("/api/users", require("./routes/api/users"));
     
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
