e1adf11fa36b4b3c5722dc95019e0560e69459bf
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2020-04-05 10:33:08 +0200

    validation using express validator

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,13 +1,23 @@
 const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require("express-validator");
 
 /*
   @rout POST api/users
   @desc Register user
   @access Public
 */
-router.post("/", (req, res) => {
-  console.log(req.body);
+router.post("/", [
+  check('name', 'name is required')
+    .not()
+    .isEmpty(),
+  check('email', 'please include a valid email').isEmail(),
+  check('password', 'please enter a password with 6 or more characters').isLength({ min: 6 })
+], (req, res) => {
+  const errors = validationResult(req);
+  if(!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
   res.send("User route");
 });
 
