ff4914c44b1b31c1e668e9884399cf3ce5ffc5f2
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2020-04-07 10:22:14 +0200

    authorise user and get user data out of db

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,12 +1,21 @@
 const express = require("express");
 const router = express.Router();
 const auth = require("../../middleware/auth");
+const User = require("../../models/User");
 
 /*
   @rout GET api/auth
   @desc Test route
   @access Public
 */
-router.get("/", auth, (req, res) => res.send("Auth route"));
+router.get("/", auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+    await res.json(user);
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).send("🔥 Server Error")
+  }
+});
 
 module.exports = router;
