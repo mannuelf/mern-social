4fb66a8674d67426d47a27f959bcec1e22ae7d7f
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2020-04-08 07:56:42 +0200

    create Profile model

---
+++ models/Profile.js
@@ -0,0 +1,112 @@
+const mongoose = require("mongoose");
+
+const ProfileSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId, // points to the _id field in record.
+    ref: "user" // create reference to the User model, every profile should be associated with a user.
+  },
+  website: {
+    type: String
+  },
+  location: {
+    type: String
+  },
+  status: {
+    type: String
+  },
+  skills: {
+    type: [String],
+    required: true
+  },
+  bio: {
+    type: String
+  },
+  githubusername: {
+    type: String
+  },
+  experience: [
+    {
+      title: {
+        type: String,
+        required: true
+      },
+      company: {
+        type: String,
+        required: true
+      },
+      location: {
+        type: String,
+        required: true
+      },
+      from: {
+        type: String,
+        required: true
+      },
+      to: {
+        type: String,
+        required: true
+      },
+      current: {
+        type: Boolean,
+        required: false
+      },
+      description: {
+        type: String
+      }
+    }
+  ],
+  education: [
+    {
+      school: {
+        type: String,
+        required: true
+      },
+      degree: {
+        type: String,
+        required: true
+      },
+      fieldofstudy: {
+        type: String,
+        required: true
+      },
+      from: {
+        type: String,
+        required: true
+      },
+      to: {
+        type: String,
+        required: true
+      },
+      current: {
+        type: Boolean,
+        required: false
+      },
+      description: {
+        type: String
+      }
+    }
+  ],
+  social: {
+    youtube: {
+      type: String
+    },
+    twitter: {
+      type: String
+    },
+    facebook: {
+      type: String
+    },
+    linkedin: {
+      type: String
+    },
+    instagram: {
+      type: String
+    }
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = Profile = mongooese.model("profile", ProfileSchema);
--- models/User.js
+++ models/User.js
@@ -21,6 +21,6 @@ const UserSchema = new mongoose.Schema({
         type: Date,
         default: Date.now
     }
-})
+});
 
 module.exports = User = mongoose.model("user", UserSchema);
