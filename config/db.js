79fd3910f2f7c9feda47e32200778dc2e07b7af2
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2020-04-09 09:06:30 +0200

    get all profiles route

--- config/db.js
+++ config/db.js
@@ -3,17 +3,18 @@ const config = require("./services");
 const db = config.mongoURI;
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(db, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            useCreateIndex: true
-        });
-        console.log("💿 Mongo DB connected");
-    } catch(err) {
-        console.log(err);
-        process.exit(1);
-    }
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    });
+    console.log("💿 Mongo DB connected");
+  } catch (err) {
+    console.log(err);
+    process.exit(1);
+  }
 };
 
 module.exports = connectDB;
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -97,6 +97,15 @@ router.post("/", [auth, validateProfileInputs], async (req, res) => {
   @desc Get all profiles
   @access Public
 */
+router.get("/", async (req, res) => {
+  try {
+    const profiles = await Profile.find().populate("user", ["name", "avatar"]);
+    await res.json(profiles);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("server Error");
+  }
+});
 
 
 module.exports = router;
