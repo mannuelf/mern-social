04bea1382a62f33a1c4772f64a30338f5cc3c69b
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2020-04-10 09:16:08 +0200

    get users github repos endpoint

--- config/services.js
+++ config/services.js
@@ -1,4 +1,8 @@
 module.exports = {
   "mongoURI": process.env.MONGO_URI,
-  "jwtSecret": process.env.JWT_SECRET
+  "jwtSecret": process.env.JWT_SECRET,
+  "github": {
+    "clientId": process.env.GITHUB_CLIENT_ID,
+    "clientSecret": process.env.GITHUB_CLIENT_SECRET
+  }
 };
