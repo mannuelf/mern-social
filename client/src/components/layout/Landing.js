97e8883687fd5ff3d6ba632b7e297e01cc6cac3e
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2020-04-12 14:38:54 +0200

    wip

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Link} from "react-router-dom";
+
 const Landing = () => {
   return (
     <section className="landing">
@@ -11,8 +12,8 @@ const Landing = () => {
             other developers
           </p>
           <div className="buttons">
-            <Link to="register" className="btn btn-primary">Sign Up</Link>
-            <Link to="login" className="btn btn-secondary">Login</Link>
+            <Link to="/register" className="btn btn-primary">Sign Up</Link>
+            <Link to="/login" className="btn btn-secondary">Login</Link>
           </div>
         </div>
       </div>
--- client/src/components/layout/NavBar.js
+++ client/src/components/layout/NavBar.js
@@ -5,11 +5,13 @@ const NavBar = () => {
   return (
     <nav className="navbar bg-dark">
       <h1>
-        <Link to="/"><i className="fas fa-code"></i> DevConnector</Link>
+        <Link to="/">
+          <i className="fas fa-code"></i> DevConnector
+        </Link>
       </h1>
       <ul>
         <li>
-          <Link to="/profiles">Developers</Link>
+          <Link to="/">Developers</Link>
         </li>
         <li>
           <Link to="/register">Register</Link>
