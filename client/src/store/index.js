c1884442ccc0f81e5f3205bd5d6d5595b9aff231
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2020-04-13 14:45:17 +0200

    store created

---
+++ client/src/store/index.js
@@ -0,0 +1,16 @@
+import { createStore, applyMiddleware} from "redux";
+import {composeWithDevTools} from "redux-devtools-extension";
+import thunk from "redux-thunk";
+import rootReducer from "./reducers/index";
+
+const initState = {};
+const middleware = [thunk];
+let store;
+
+store = createStore(
+  rootReducer,
+  initState,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
+
+export default store;
