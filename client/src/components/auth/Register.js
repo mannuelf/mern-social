ffad133ee1f0d342492c361ef0eccca3697fcd25
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2020-04-13 14:13:15 +0200

    login form, basic basic login on fe, must move to be.

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,5 +1,6 @@
 import React, {Fragment, useState} from 'react';
 import {Link} from "react-router-dom";
+import Axios from "axios";
 
 const Register = () => {
   const [formData, setFormData] = useState({
@@ -11,15 +12,55 @@ const Register = () => {
 
   const {name, email, password, password2} = formData;
 
+  const onChange = e =>
+    setFormData({...formData, [e.target.name]: e.target.value});
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    if (password !== password2) {
+      console.log("passwords do not match")
+    } else {
+      const newUser = {
+        name,
+        email,
+        password
+      }
+      try {
+        const config = {
+          headers: {
+            "Content-Type": "application/json"
+          }
+        }
+        const body = JSON.stringify(newUser);
+        const res = await Axios.post("/api/users", body, config);
+      } catch(err) {
+        console.error(err.response);
+      }
+    }
+  };
+
   return <Fragment>
     <h1 className="large text-primary">Sign Up</h1>
     <p className="lead"><i className="fas fa-user"></i> Create Your Account</p>
-    <form className="form" action="create-profile.html">
+    <form className="form" onSubmit={e => onSubmit(e)}>
       <div className="form-group">
-        <input type="text" placeholder="Name" name="name" required/>
+        <input
+          type="text"
+          placeholder="Name"
+          name="name"
+          value={name}
+          onChange={e => onChange(e)}
+          required
+        />
       </div>
       <div className="form-group">
-        <input type="email" placeholder="Email Address" name="email"/>
+        <input
+          type="email"
+          placeholder="Email Address"
+          name="email"
+          value={email}
+          onChange={e => onChange(e)}
+        />
         <small className="form-text"
         >This site uses Gravatar so if you want a profile image, use a
           Gravatar email</small
@@ -31,6 +72,8 @@ const Register = () => {
           placeholder="Password"
           name="password"
           minLength="6"
+          value={password}
+          onChange={e => onChange(e)}
         />
       </div>
       <div className="form-group">
@@ -39,6 +82,8 @@ const Register = () => {
           placeholder="Confirm Password"
           name="password2"
           minLength="6"
+          value={password2}
+          onChange={e => onChange(e)}
         />
       </div>
       <input type="submit" className="btn btn-primary" value="Register"/>
