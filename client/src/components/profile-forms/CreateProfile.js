486aa8848b750fdd595ad8b5014a957299d2b553
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2020-04-19 19:19:24 +0200

    create profile

---
+++ client/src/components/profile-forms/CreateProfile.js
@@ -0,0 +1,16 @@
+import React, {useState} from 'react';
+import {connect} from "react-redux";
+import PropTypes from 'prop-types';
+
+const CreateProfile = props => {
+  const [formData, setFormData] = useState({});
+  return (
+    <div>
+
+    </div>
+  );
+};
+
+CreateProfile.propTypes = {};
+
+export default CreateProfile;
