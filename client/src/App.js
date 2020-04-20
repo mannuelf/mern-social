9633808b23643d4a792c5b006016b49f9ed36c3f
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2020-04-20 21:37:15 +0200

    create profile form

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import Login from "./components/auth/Login";
 import Alert from "./components/layout/alert";
 import Dashboard from "./components/dashboard/Dashboard";
 import PrivateRoute from "./components/routing/PrivateRoute";
+import CreateProfile from "./components/profile-forms/CreateProfile";
 import {Provider} from "react-redux";
 import store from "./store/index";
 import {loadUser} from "./store/actions/auth";
@@ -33,6 +34,7 @@ const App = () => {
             <Route exact path="/register" component={Register}/>
             <Route exact path="/login" component={Login}/>
             <PrivateRoute exact path="/dashboard" component={Dashboard}/>
+            <PrivateRoute exact path="/create-profile" component={CreateProfile}/>
           </Switch>
         </section>
       </Fragment>
