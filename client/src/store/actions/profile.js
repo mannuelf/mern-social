9be8cf81bfc44c9197554bc3f7b3411dc01886f5
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2020-04-20 21:45:39 +0200

    create action to get the data from form, handle errors

--- client/src/store/actions/profile.js
+++ client/src/store/actions/profile.js
@@ -18,3 +18,40 @@ export const getCurrentProfile = () => async dispatch => {
   }
 }
 
+// Create of update profile
+export const createProfile = (formData, history, edit = false) => async dispatch => {
+  try {
+    const config = {
+      headers: {
+        "Content-Type": "application/json"
+      }
+    }
+
+    const res = await Axios.post("/api/profile", formData, config);
+
+    dispatch({
+      type: GET_PROFILE,
+      payload: res.data
+    })
+
+    dispatch(setAlert(edit ? "Profile Updated 🚀" : "Profile Created"));
+
+    // go to the dashboard page if not editing.
+    if (!edit) {
+      history.push("/dashboard");
+    }
+  } catch (err) {
+    console.log(err.message);
+    const errors = err.response.data.errors;
+
+    if (errors) {
+      errors.forEach(error => dispatch(setAlert(error.msg, "danger")));
+    }
+
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: {msg: err.response.statusText, status: err.response.status}
+    })
+  }
+}
+
