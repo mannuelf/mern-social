e014a57d1d33c8582e1cb93931d44bd879508013
Mannuel Ferreira <mannuel.ferreira@gmail.com>
2020-04-25 07:50:14 +0200

    delete education and experiences from user

--- client/src/store/actions/profile.js
+++ client/src/store/actions/profile.js
@@ -129,3 +129,38 @@ export const addEducation = (formData, history) => async dispatch => {
   }
 }
 
+// Delete an experience
+export const deleteExperience = id => async dispatch => {
+  try {
+    const res = await Axios.delete(`/api/profile/experience/${id}`);
+    dispatch({
+      type: UPDATE_PROFILE,
+      payload: res.data
+    });
+
+    dispatch(setAlert("Experience removed", "success"))
+  } catch(err) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: {msg: err.response.statusText, status: err.response.status}
+    });
+  }
+}
+
+// Delete education
+export const deleteEducation = id => async dispatch => {
+  try {
+    const res = await Axios.delete(`/api/profile/experience/${id}`);
+    dispatch({
+      type: UPDATE_PROFILE,
+      payload: res.data
+    });
+
+    dispatch(setAlert("Education removed", "success"));
+  } catch(err) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: {msg: err.response.statusText, status: err.response.status}
+    })
+  }
+}
